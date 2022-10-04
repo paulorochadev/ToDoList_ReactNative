1e23f3d45f77eeb9dfba2923e9c1be36d21532d9
paulo.rocha <paulo.rocha@hotmail.com>
2022-10-04 02:27:27 -0300

    Inclusão dos Estilos da Home

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,18 +3,45 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: '#1A1A1A',
+    flex: 1,
+    // padding: 24,
   },
   form: {
-    
+    alignSelf: 'center',
+    flexDirection: "row",
+    marginTop: -28,
+    width: 327,
+
+    // marginBottom: 42,
+    // marginTop: 36,
+    // width: '100%',
   },
   input: {
-
+    backgroundColor: '#262626',
+    borderRadius: 5,
+    color: '#FFF',
+    flex: 1,
+    fontSize: 16,
+    height: 56,
+    marginRight: 10,
+    padding: 16,
+    
+    // backgroundColor: '#1F1E25',
+    // marginRight: 12,
   },
   button: {
+    alignItems: 'center',
+    backgroundColor: '#1E6F9F',
+    borderRadius: 5,
+    height: 56,
+    justifyContent: 'center',
+    width: 56,
 
+    // backgroundColor: '#31CF67',
   },
   buttonText: {
-    
+    color: '#FFF',
+    fontSize: 24,
   },
 });
