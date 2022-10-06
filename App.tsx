5eef8ed3b3f669c20f01cbf6097b5acee21b5d93
paulo.rocha <paulo.rocha@hotmail.com>
2022-10-06 02:49:38 -0300

    Ajustes de build

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from 'react-native';
 
-import { Header } from './src/components/Header';
 import { Home } from './src/screens/Home';
 
 export default function App() {
@@ -10,15 +9,8 @@ export default function App() {
         barStyle='light-content'
         backgroundColor='#0d0d0d'
         translucent={false}
-
-        /* ---- VALIDATION ----
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-        */
       />
       
-      {/* <Header /> */}
       <Home />
     </>
   );
